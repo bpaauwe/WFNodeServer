fb939f035b5a4220176b381c5d20cde55912918a
Bob Paauwe <bpaauwe@bobsplace.com>
2018-03-22 11:45:10 -0700

    Clean up the rest request code.
    Simplify the code a bit and try to make it behave better.

--- WFNodeServer/wf_rest.cs
+++ WFNodeServer/wf_rest.cs
@@ -98,88 +98,57 @@ namespace WFNodeServer {
 
             //Console.WriteLine(rest_url);
             request = (HttpWebRequest)HttpWebRequest.Create(rest_url);
+            request.UserAgent = "WFNodeServer";
             if (AuthRequired)
                 request.Headers.Add("Authorization", AuthHeader);
             request.Proxy = null;
             request.KeepAlive = false;
 
             // Read data from the stream
-            try {
-                response = (HttpWebResponse)request.GetResponse();
-                if (response.ContentLength > 0) {
-                    code = (int)response.StatusCode;
-                    if (code != 200) {
-                        if (code == 404) {
-                            // This isn't really an error, it just means
-                            // there's no entries at this URL
-                            Console.WriteLine("REST request " +
-                                    url + " failed " +
-                                    response.StatusDescription);
-                        } else if (code == 405) {
-                            // This isn't really an error. It means that
-                            // the URL doesn't exist (mostly seen when
-                            // trying to query information about a module that
-                            // is not installed.
-                            Console.WriteLine("REST request " +
-                                    url + " failed " +
-                                    response.StatusDescription);
-                        } else {
-                            Console.WriteLine("ISY REST request to URL, " +
-                                    url + ", failed with " +
-                                    response.StatusDescription);
-                        }
-                    } else {  // code == 200
-                        byte[] buf = new byte[4096];
-                        int len;
-                        string chunk;
-
-                        reader = response.GetResponseStream();
-                        try {
-                            do {
-                                len = reader.Read(buf, 0, buf.Length);
-                                //Console.WriteLine("  -> Read " + len.ToString() + " bytes from stream.");
-                                if (len > 0) {
-                                    chunk = new String(Encoding.ASCII.GetString(buf).ToCharArray(), 0, len);
-                                    xml += chunk;
-                                }
-                            } while (len > 0);
-                        } catch {
-                            Console.WriteLine("Ignoring reader exception?");
-                        }
-
-                        reader.Close();
-                        //Console.WriteLine("REST got: " + xml);
-                    }
-                } // End of content length > 0
+            using (response = (HttpWebResponse)request.GetResponse()) {
                 try {
-                    response.Close();
-                } catch {
-                    Console.WriteLine("Ignoring exception on close?");
-                }
-            } catch (WebException ex) {
-                // Try to get more information about the error
-                if (ex.Response != null) {
-                    HttpWebResponse errorResponse = (HttpWebResponse)ex.Response;
-                    code = (int)errorResponse.StatusCode;
-                    if (code == 200) {
-                        xml = ex.Message;
-                    } else if (code == 404) {
-                        // Not found errors are not really errors.
-                        Console.WriteLine("REST request " +
-                                url + " failed {" + code.ToString() + "} " +
-                                errorResponse.StatusDescription);
-                        xml = ex.Message;
-                    } else {
-                        Console.WriteLine("ISY REST request to URL, " +
-                                url + ", failed with {" + code.ToString() + "} " +
-                                errorResponse.StatusDescription);
-                    }
-                } else {
-                    xml = "ISY REST request " + url + " failed with " + ex.Message;
+                    if (response.ContentLength > 0) {
+                        code = (int)response.StatusCode;
+                        if (code != 200) {
+                            if (code == 404) {  // No entries match this request
+                                Console.WriteLine("REST request " + url + " failed " + response.StatusDescription);
+                            } else if (code == 405) { // URL doesn't exist (bad request)
+                                Console.WriteLine("REST request " + url + " failed " + response.StatusDescription);
+                            } else {
+                                Console.WriteLine("ISY REST request to URL, " +
+                                        url + ", failed with " +
+                                        response.StatusDescription);
+                            }
+                        } else {  // code == 200 OK, read response
+                            byte[] buf = new byte[4096];
+                            int len;
+                            string chunk;
+
+                            reader = response.GetResponseStream();
+                            try {
+                                do {
+                                    len = reader.Read(buf, 0, buf.Length);
+                                    //Console.WriteLine("  -> Read " + len.ToString() + " bytes from stream.");
+                                    if (len > 0) {
+                                        chunk = new String(Encoding.ASCII.GetString(buf).ToCharArray(), 0, len);
+                                        xml += chunk;
+                                    }
+                                } while (len > 0);
+                            } catch {
+                                Console.WriteLine("Ignoring reader exception?");
+                            }
+
+                            reader.Close();
+                            //Console.WriteLine("REST got: " + xml);
+                        }
+                    } // End of content length > 0
+                } catch (WebException ex) {
+                    xml = "";
                     //Console.WriteLine(xml);
                     //throw new RestException();
                     Console.WriteLine(ex.Message);
                 }
+                response.Close();
             }
 
             request.Abort();
